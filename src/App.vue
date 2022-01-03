cf236aaa0c48c9d47e35b7750bd2e188a07db786
Christie Maduna <ferggiee@gmail.com>
2022-01-03 09:23:42 +0200

    commit:

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,50 @@
 <template>
   <div class="textarea-emoji-picker">
-    <textarea
+    <!-- <textarea
       ref="textarea"
       class="textarea"
       :value="value"
       @input="$emit('input', $event.target.value)"
-    ></textarea>
-    <picker
-      v-show="showEmojiPicker"
-      title="Pick your emoji..."
-      :data="emojiIndex"
-      emoji="point_up"
-      @select="showEmoji"
-    />
-    </div>
+    >
+    </textarea> -->
     <div class="row">
-    <div>
-      {{ emojisOutput }}
+      <Picker
+        v-show="showEmojiPicker"
+        title="Pick your emoji..."
+        :data="emojiIndex"
+        :include="['people']"
+        native
+        :show-categories="false"
+        :show-search="false"
+        set="twitter"
+        emoji="point_up"
+        @select="showEmoji"
+      />
     </div>
-      <svg style="width: 20px; height: 20px" viewBox="0 0 24 24">
-        <path
-          fill="#888888"
-          d="M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M10,9.5C10,10.3 9.3,11 8.5,11C7.7,11 7,10.3 7,9.5C7,8.7 7.7,8 8.5,8C9.3,8 10,8.7 10,9.5M17,9.5C17,10.3 16.3,11 15.5,11C14.7,11 14,10.3 14,9.5C14,8.7 14.7,8 15.5,8C16.3,8 17,8.7 17,9.5M12,17.23C10.25,17.23 8.71,16.5 7.81,15.42L9.23,14C9.68,14.72 10.75,15.23 12,15.23C13.25,15.23 14.32,14.72 14.77,14L16.19,15.42C15.29,16.5 13.75,17.23 12,17.23Z"
-        />
-      </svg>
+
+    <div class="row">
+      <div>
+        {{ emojisOutput }}
+      </div>
+      <span
+        class="emoji-trigger"
+        :class="{ triggered: showEmojiPicker }"
+        @mousedown.prevent="toggleEmojiPicker"
+      >
+        <svg style="width: 20px; height: 20px" viewBox="0 0 24 24">
+          <path
+            fill="#888888"
+            d="M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M10,9.5C10,10.3 9.3,11 8.5,11C7.7,11 7,10.3 7,9.5C7,8.7 7.7,8 8.5,8C9.3,8 10,8.7 10,9.5M17,9.5C17,10.3 16.3,11 15.5,11C14.7,11 14,10.3 14,9.5C14,8.7 14.7,8 15.5,8C16.3,8 17,8.7 17,9.5M12,17.23C10.25,17.23 8.71,16.5 7.81,15.42L9.23,14C9.68,14.72 10.75,15.23 12,15.23C13.25,15.23 14.32,14.72 14.77,14L16.19,15.42C15.29,16.5 13.75,17.23 12,17.23Z"
+          />
+        </svg>
+      </span>
+    </div>
   </div>
 </template>
-
 
 <script>
 import data from "emoji-mart-vue-fast/data/all.json";
 
-// Note: component needs to be imported from /src subfolder:
 import { Picker, EmojiIndex } from "emoji-mart-vue-fast/src";
 import "emoji-mart-vue-fast/css/emoji-mart.css";
 
@@ -119,5 +132,4 @@ export default {
 .emoji-trigger.triggered path {
   fill: darken(#fec84a, 15%);
 }
-
 </style>
